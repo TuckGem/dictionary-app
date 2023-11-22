b321f5b6618a08c5a50362090bc75dbad53eed48
Catarina da Rosária <catarinadarosaria@gmail.com>
2023-11-22 15:16:12 +0100

    added api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
+  function showDefinition(response) {
+    console.log(response.data);
   }
 
   function handleResponse(event) {
     event.preventDefault();
-    alert(`Searching for the word ${keyword}`);
+
+    let apiKey = "84d76171da4e217dad7t0o3ca0d5af7b";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(showDefinition);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
   }
   return (
     <div className="Dictionary">
