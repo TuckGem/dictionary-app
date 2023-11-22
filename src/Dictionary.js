bc3c22f4f6f3c5ad1db9f0e13b242ea52027ec70
Catarina da Rosária <catarinadarosaria@gmail.com>
2023-11-22 23:16:02 +0100

    images api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState(null);
 
   function showDefinition(response) {
     setResults(response.data);
+  }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function handleResponse(event) {
@@ -17,6 +22,11 @@ export default function Dictionary() {
     let apiKey = "84d76171da4e217dad7t0o3ca0d5af7b";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(showDefinition);
+
+    let imagesApiKey = "84d76171da4e217dad7t0o3ca0d5af7b";
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}`;
+
+    axios.get(imagesApiUrl).then(handlePexelsResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -33,9 +43,10 @@ export default function Dictionary() {
             onChange={handleKeywordChange}
           />
         </form>
-        <small class="hint">i.e. paris, wine, yoga, coding</small>
+        <small className="hint">i.e. paris, wine, yoga, coding</small>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
